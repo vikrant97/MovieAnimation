7027809e685fbc0c244ae115119f76edf9ce480e
vikrant goyal <vikrantgoyal97@gmail.com>
2017-03-27 22:18:36 +0530

    final

---
+++ PT_scene/Assets/FinalCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Runner1;
+
+	private Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - Runner1.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = Runner1.transform.position + offset;
+	}
+}
---
+++ PT_scene/Assets/FollowCam.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+	public GameObject runner1;
+
+	private Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - runner1.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = runner1.transform.position + offset;
+	}
+}
